64ddf060a1d1eb2879d42a5811b9b640dbf34993
Nguyen Quang Anh <quanganh598912@gmail.com>
2024-11-18 12:50:28 +0700

    update ui

---
+++ Assets/Scripts/Button/btnStartGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btnStartGame : BaseBtn
+{
+    protected override void OnClick() {
+        SceneManager.LoadScene(1);
+    }
+}
